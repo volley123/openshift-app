09be53970b598185380ea164531fc87975a9a5cb
volley123 <37272568+volley123@users.noreply.github.com>
2021-11-02 17:48:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Flask python application running on openshift cluster'
     
 @app.route('/how are you')
 def hello():
      return 'I am good, how about you?'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
