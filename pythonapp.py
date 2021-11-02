cc2d0cda0b4b8531afeebbf11d5c3e04fdf55d39
volley123 <37272568+volley123@users.noreply.github.com>
2021-11-02 15:40:04 +0530

    Create pythonapp.py

---
+++ pythonapp.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+    
+@app.route('/how are you')
+def hello():
+     return 'I am good, how about you?'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
